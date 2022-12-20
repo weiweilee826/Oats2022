648a80f0340d45fcfd54e88320b6ac853887fcab
weiwei <weiweilee826@gmail.com>
2022-12-20 11:46:04 +0800

    設定路徑

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { requiresAuth: true },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
