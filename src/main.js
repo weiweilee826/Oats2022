8837619d89a154599bcd3b694d10ccaa0febd7e8
weiwei <weiweilee826@gmail.com>
2022-12-20 11:45:11 +0800

    安裝外掛

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+const app = createApp(App);
+app.use(router).use(BootstrapVue3).mount("#app");
