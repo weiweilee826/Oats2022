e794058bd94b74d316ae653bb55db39657c304d5
weiwei <weiweilee826@gmail.com>
2022-12-31 16:45:49 +0800

    新增元件 NavbarAdmin.vue

---
+++ src/components/NavbarAdmin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-nav pills align="center">
+      <b-nav-item>
+        <!-- <router-link to="/products">產品列表</router-link> -->
+      </b-nav-item>
+      <b-nav-item>訂單列表</b-nav-item>
+      <b-nav-item>優惠券</b-nav-item>
+      <b-nav-item>
+        <router-link to="/">logo</router-link>
+      </b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
