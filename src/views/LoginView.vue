418f277a14229c5583555f3125b548b8c2d30573
weiwei <weiweilee826@gmail.com>
2022-12-31 16:42:40 +0800

    新增 LoginView.vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="form-bg">
+        <form class="form-signin" @submit.prevent="signin">
+          <!-- <img
+            class="mb-4"
+            src="/docs/5.2/assets/brand/bootstrap-logo.svg"
+            alt=""
+            width="72"
+            height="57"
+          /> -->
+          <h1 class="h3 mb-3 fw-normal">登入</h1>
+
+          <div class="form-floating">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+              v-model="user.username"
+              required
+            />
+            <label for="floatingInput">電子郵件</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+              v-model="user.password"
+              required
+            />
+            <label for="floatingPassword">密碼</label>
+          </div>
+
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">
+            登入
+          </button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2022–2023</p>
+        </form>bootstrap-logo.svg
+    </div>
+  </div>
+</template>
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    signin() {
+      const api = `${process.env.VUE_APP_API}/admin/signin`;
+      console.log(this.$http.id)
+      this.$http.post(api, this.user).then((response) => {
+        console.log("456", response);
+        if (response.data.success) {
+          const { token, expired } = response.data;
+          document.cookie = `hexToken=${token}; expires=${expired}`;
+          this.$router.push("/");
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+template {
+  background-color: #ec6d4e;
+}
+.form-bg {
+  width: 50%;
+  display: -ms-flexbox;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+  background-color: rgb(196, 217, 186);
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
