1bac0d79eec6900fbf37459652f191e51d84393d
weiwei <weiweilee826@gmail.com>
2022-12-31 16:45:06 +0800

    新增 AdminView.vue

---
+++ src/views/AdminView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavbarAdmin />
+    <div></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarAdmin from "@/components/NavbarAdmin.vue";
+
+export default {
+  name: "Admin",
+  components: {
+    NavbarAdmin,
+  },
+};
+</script>
