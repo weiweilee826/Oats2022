2baa14c46777f76017426a5b96a761af5839a2e0
weiwei <weiweilee826@gmail.com>
2022-12-31 16:44:36 +0800

    新增 ProductView.vue

---
+++ src/views/ProductView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavBar />
+    <div></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "ProductView",
+  components: {
+    NavBar,
+  },
+};
+</script>
