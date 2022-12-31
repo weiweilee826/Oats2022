a08d3df4008e85f08ef59232434a5713cdbeecc4
weiwei <weiweilee826@gmail.com>
2022-12-31 16:40:37 +0800

    設定路徑

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: Home,
     meta: { requiresAuth: true },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/LoginView.vue"),
+    // meta: { requiresAuth: false },
+  },
+  {
+    path: "/products",
+    name: "products",
+    component: () => import("../views/ProductsView.vue"), 
+  },
+  {
+    path: "/product",
+    name: "product",
+    component: () => import("../views/ProductView.vue"),
   },
 ];
 
