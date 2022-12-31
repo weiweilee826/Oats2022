0ddb79ebd969fbd6dca1a05f6b17702fcc5158cd
weiwei <weiweilee826@gmail.com>
2022-12-31 16:40:16 +0800

    安裝外掛

--- src/main.js
+++ src/main.js
@@ -6,5 +6,16 @@ import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
+import axios from "axios";
+import VueAxios from "vue-axios";
+
 const app = createApp(App);
-app.use(router).use(BootstrapVue3).mount("#app");
+app
+  .use(router)
+  .use(BootstrapVue3)
+  .use(VueAxios, axios)
+  //.provide("axios", app.config.globalProperties.axios)
+  .mount("#app");
+
+//標題名稱
+document.title = "Oats";
