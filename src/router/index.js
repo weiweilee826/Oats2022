3ad8d1b61aa96c43dfbc50b9996ae924891f96d6
weiwei <weiweilee826@gmail.com>
2023-02-10 11:05:46 +0800

    新增路徑 admin

--- src/router/index.js
+++ src/router/index.js
@@ -17,15 +17,36 @@ const routes = [
   {
     path: "/products",
     name: "products",
-    component: () => import("../views/ProductsView.vue"), 
+    component: () => import("../views/ProductsView.vue"),
   },
   {
     path: "/product",
     name: "product",
     component: () => import("../views/ProductView.vue"),
   },
+  {
+    path: "/admin",
+    name: "admin",
+    component: () => import("../views/AdminView.vue"),
+    children: [
+      {
+        path: "product_list",
+        name: "productList",
+        component: () => import("../views/ProductList.vue"),
+      },
+      // {
+      //   path: "order_list",
+      //   name: "orderList",
+      //   component: () => import("../views/OrderList.vue"),
+      // },
+      // {
+      //   path: "coupon",
+      //   name: "couponView",
+      //   component: () => import("../views/CouponView.vue"),
+      // },
+    ],
+  },
 ];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
