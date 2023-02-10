7da8f934543a140e353b58f957e9f82ca6a2c042
weiwei <weiweilee826@gmail.com>
2023-02-10 10:46:55 +0800

    安裝外掛 fontawesome

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import BootstrapVue3 from "bootstrap-vue-3";
+// import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+// import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+// icon
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+library.add(faSpinner, faTrash, faCartShopping);
 
 import axios from "axios";
 import VueAxios from "vue-axios";
@@ -12,10 +20,21 @@ import VueAxios from "vue-axios";
 const app = createApp(App);
 app
   .use(router)
-  .use(BootstrapVue3)
+  // .use(BootstrapVue3)
   .use(VueAxios, axios)
   //.provide("axios", app.config.globalProperties.axios)
+  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
+
+  //filter
+app.config.globalProperties.$filters = {
+  currencyUSD(value) {
+    return "$" + value;
+  },
+  timestampToDate(t) {
+    return new Date(t).toLocaleDateString("fr-ca");
+  },
+};
 
 //標題名稱
 document.title = "Oats";
