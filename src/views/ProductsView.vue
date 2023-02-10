fae02d1091469bf4a0b524ece8bc840127264437
weiwei <weiweilee826@gmail.com>
2023-02-10 11:04:23 +0800

    新增 取得商品列表

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,67 +1,31 @@
 <template>
-  <div>
+  <div style="background-color: #ec6d4e">
+    <loading :active="isLoading" />
     <NavBar />
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
-            <div class="card shadow-sm">
-              <img src="../assets/images/products/orange.webp" alt="">
-              <!-- <svg
-                class="bd-placeholder-img card-img-top"
-                width="10%"
-                height="10%"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-              </svg> -->
 
-              <div class="card-body">
-                <p class="card-text">
-                  “FruOats” 草莓開心果燕麥曲奇 30片
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      View
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small class="text-muted">685 元</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
+    <div class="container content">
+      <div class="container-md">
+        <h1 class="title">PRODUCTS</h1>
+        <div class="row row-cols-4 g-3">
+          <div
+            class="col"
+            style="width: 24%"
+            v-for="item in products"
+            :key="item.id"
+          >
             <div class="card shadow-sm">
-              <img src="../assets/images/products/starberry.webp" alt="">
+              <router-link to="/product">
+                <img src="../assets/images/products/flavor_01.webp" alt=""
+              /></router-link>
               <div class="card-body">
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
+                <p class="card-text">{{ item.title }}</p>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                     <button
                       type="button"
                       class="btn btn-sm btn-outline-secondary"
                     >
-                      View
+                      更多
                     </button>
                     <button
                       type="button"
@@ -70,7 +34,7 @@
                       Edit
                     </button>
                   </div>
-                  <small class="text-muted">9 mins</small>
+                  <small class="text-muted">{{ item.price }} 元</small>
                 </div>
               </div>
             </div>
@@ -82,7 +46,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
 
 export default {
@@ -90,5 +53,63 @@ export default {
   components: {
     NavBar,
   },
+  data() {
+    return {
+      products: [],
+      tempProduct: {},
+      myModal: {},
+      isNew: false,
+      isLoading: false,
+      status: {
+        fileUploading: false,
+      },
+    };
+  },
+  methods: {
+    getProducts() {
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/products`;
+      this.isLoading = true;
+      this.$http.get(api).then((response) => {
+        console.log(response);
+        if (response.data.success) {
+          this.isLoading = false;
+          this.products = response.data.products;
+        }
+      });
+    },
+  },
+  created() {
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    if (token !== "") {
+      this.axios.defaults.headers.common["Authorization"] = token;
+      this.getProducts();
+    }
+  },
 };
 </script>
+
+<style scoped>
+.content {
+  margin-top: 100px;
+  background-color: #ec6d4e;
+}
+img {
+  width: 228px;
+}
+.title {
+  /* font-family: futura-pt, sans-serif; */
+  font-weight: 300;
+  font-style: normal;
+  font-size: 50px;
+  line-height: 1;
+  letter-spacing: 10px;
+  color: #fff;
+  text-align: center;
+  margin: 0px auto 60px;
+  padding-top: 100px;
+  overflow: hidden;
+}
+</style>
