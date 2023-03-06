5618745178b6a90acb3086ca6d214b23e5ddf8c1
weiwei <weiweilee826@gmail.com>
2023-03-06 10:25:34 +0800

    新增 FooterMain.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
+  <NavBar />
   <router-view />
+  <FooterMain />
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
+import FooterMain from "@/components/FooterMain.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    
-  }
-}
+    NavBar,
+    FooterMain,
+  },
+};
 </script>
 
 <style>
@@ -19,6 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
