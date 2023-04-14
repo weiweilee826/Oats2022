584648e030ce1110a3896bb2329a26de0c35266d
weiwei <weiweilee826@gmail.com>
2023-04-14 16:44:45 +0800

    新增首頁圖文

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,697 @@
 <template>
-  <div class="main1">
-    <div></div>
+  <div>
+    <div class="container-fluid p-0 index">
+      <!-- section 1 -->
+      <div class="slider-content">
+        <div class="slider-text">
+          <p>Vegan Friendly</p>
+          <h3>キレイのもとは、<br />おいしいおやつ。</h3>
+        </div>
+
+        <div class="slider">
+          <div class="index-slogan">
+            <img
+              src="../assets/images/home_page/static.shared__logo_wh.svg"
+              alt=""
+            />
+            <img
+              src="../assets/images/home_page/static.index__tagline.svg"
+              alt=""
+              style="margin-top: 15px"
+            />
+          </div>
+
+          <div class="slider-img">
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_1.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_2.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_3.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_4.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_5.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_1.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_2.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_3.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_4.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="slider-item">
+              <img
+                src="../assets/images/home_page/static.index__slide_5.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- scroll animation -->
+      <img
+        class="scroll"
+        src="../assets/images/icons/static.index__scroll.svg"
+        alt=""
+      />
+
+      <!-- section 2 -->
+      <div class="about-content mt-5">
+        <p class="text-center content-title og">About FruOats</p>
+        <div class="about-detail">
+          <h3 class="detail-title">無須忍受的<br />美容零食</h3>
+          <p>
+            隨著健康和美容意識的增強，<br />
+            越來越多的人選擇健康零食或不吃零食。
+            <br /><br />
+            我們通過僅嚴格選擇簡單的植物來源成分來開發 FruOats，<br />
+            以便任何人都可以享受零食時間而不會感到內疚。
+            <br /><br />
+            一種富含膳食纖維、維生素和礦物質的無麩質餅乾，<br />
+            由作為超級食品備受關注的燕麥片和奇亞籽以及豆渣和乾果製成。<br />
+            請享用平衡美味與美麗的快樂小吃。
+          </p>
+        </div>
+      </div>
+
+      <!-- section 3 -->
+      <div class="marquee-content mt-5">
+        <div class="eyecatch">
+          <p>
+            Strawberry. Pistachio. Mango. Coconut. Apple. Cinnamon. Fig. Almond.
+            Orange. Chocolate.
+          </p>
+        </div>
+      </div>
+
+      <!-- section 4 -->
+      <div class="feature-content">
+        <p class="text-center content-title">FruOats’s Feature</p>
+
+        <ul class="feature-list">
+          <li class="feature-item">
+            <p class="item-number">1</p>
+            <img src="../assets/images/home_page/index_01.jpg" alt="" />
+
+            <div class="feature-detail">
+              <h3 class="feature-title">用植物來源的材料<br />由內而外美麗</h3>
+              <p class="feature-text">
+                我們不使用糖果中經常使用的雞蛋、黃油、麵粉或白糖，而是使用所有植物來源的成分，如燕麥片、豆渣粉、奇亞籽、亞麻籽、乾果和米油。此外，不使用人工甜味劑和防腐劑，是一款對身體溫和的曲奇。
+              </p>
+            </div>
+          </li>
+          <li class="feature-item">
+            <p class="item-number">2</p>
+            <img src="../assets/images/home_page/index_02.jpg" alt="" />
+
+            <div class="feature-detail">
+              <h3 class="feature-title">
+                3 塊 1/2 天的 膳食纖維碳水化合物約 51% 的折扣
+              </h3>
+              <p class="feature-text">
+                與普通餅乾相比，營養價值一目了然！低糖且富含膳食纖維。大多數日本人缺乏膳食纖維，20
+                多歲和 30 多歲的男性和女性平均缺乏約 6g。只需 3 片（45
+                克）水果燕麥，您就可以獲得一天所需膳食纖維的一半。這相當於大約
+                9-10 根香蕉。
+              </p>
+            </div>
+          </li>
+          <li class="feature-item">
+            <p class="item-number">3</p>
+            <img src="../assets/images/home_page/index_03.jpg" alt="" />
+
+            <div class="feature-detail">
+              <h3 class="feature-title">
+                由活躍的糕點師監督，<br />滿意率為95％*！
+              </h3>
+              <p class="feature-text">
+                為了確保您能吃到美味，我們請來了曾在“Coupe du Monde de la
+                Patisserie”等國際糕點比賽中獲獎的主廚中山和弘監製。深知製作美味健康食品的難度，我們反複試錯，在不犧牲營養和成分的情況下做出美味。<br />
+                （* 2021 年 9 月調查，41 名監測員）
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <!-- section 5 -->
+      <div class="story-content">
+        <p class="text-center content-title">Story / Promise</p>
+        <div class="content-detail">
+          <div class="story-detail">
+            <h2>以兼顧美味和美麗<br />為目標</h2>
+            <p>
+              FruOats
+              的開發始於“我不想忍受零食”的想法。為了達到健康和口感兼顧，我們歷經了數十次測試，沒有使用常見的食材。
+            </p>
+            <router-link to="/about_us"
+              ><button
+                type="button"
+                class="btn btn-outline-light rounded-pill mt-5 fw-bold story-btn"
+              >
+                品牌故事
+              </button></router-link
+            >
+          </div>
+          <div class="story-img">
+            <img
+              class="img-fluid rounded"
+              src="../assets/images/home_page/index_05.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="eyecatch">
+          <p>
+            Story. Promise. Story. Promise. Story. Promise. Story. Promise.
+            Story. Promise. Story. Promise.
+          </p>
+        </div>
+      </div>
+
+      <!-- section 6 -->
+      <div class="product-content">
+        <p class="text-center content-title">Product</p>
+        <div class="product-container">
+          <div class="product-info">
+            <!-- 疊圖 -->
+            <div class="product-img">
+              <img
+                class="img-fluid rounded top-img"
+                src="../assets/images/home_page/index_04.jpg"
+                alt=""
+              />
+              <img
+                class="img-fluid rotate-r"
+                src="../assets/images/home_page/static.index__product-deco_1_600x.jpg"
+                alt=""
+              />
+              <img
+                class="img-fluid rotate-l"
+                src="../assets/images/home_page/static.index__product-deco_2_600x.jpg"
+                alt=""
+              />
+            </div>
+
+            <div class="product-detail">
+              <h5>{{ products[0].title }}</h5>
+              <h1>FruOats</h1>
+              <p>{{ $filters.currencyUSD(products[0].price) }}</p>
+              <router-link to="'/product/' + products"
+                ><button
+                  type="button"
+                  class="btn btn-outline-light rounded-pill mt-5 fw-bold story-btn"
+                >
+                  商品詳細
+                </button></router-link
+              >
+            </div>
+          </div>
+          <!-- 商品列表 -->
+          <div class="products row">
+            <template v-for="item in products">
+              <div
+                class="col"
+                style="width: 20%"
+                v-if="item.is_enabled"
+                :key="item.id"
+              >
+                <!-- 傳參數 params -->
+                <router-link :to="'/product/' + item.id">
+                  <div class="card bg-transparent text-white border-0">
+                    <img class="rounded" :src="item.imageUrl" alt="" />
+                    <div class="card-body text-start">
+                      <p class="card-text">{{ item.title }}</p>
+                      <p class="card-text">
+                        {{ $filters.currencyUSD(item.price) }}
+                      </p>
+                    </div>
+                  </div>
+                </router-link>
+              </div>
+            </template>
+          </div>
+          <!-- 更多商品 -->
+          <div class="products-all text-end">
+            <router-link to="/products"
+              ><button type="button" class="btn btn-outline-light rounded-pill">
+                All Products
+              </button></router-link
+            >
+          </div>
+        </div>
+      </div>
+
+      <!-- section 7 -->
+      <div class="news-content">
+        <p class="text-center content-title">News</p>
+        <table class="table text-white">
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody class="news-column">
+            <tr>
+              <th scope="row">2023.04.01</th>
+              <td>【從4/6開始數量有限】“FruOats”將開始銷售“素食巧克力”！</td>
+              <td>
+                <router-link to="/news_post1"
+                  ><font-awesome-icon icon="fa-solid fa-chevron-right"
+                /></router-link>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">2023.03.21</th>
+              <td>發表於2023年3月號的雜誌《Ray》</td>
+              <td>
+                <router-link to="/news_post2"
+                  ><font-awesome-icon icon="fa-solid fa-chevron-right"
+                /></router-link>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">2023.02.08</th>
+              <td scope="row">【重要】運營公司變更、條款修改等通知</td>
+              <td>
+                <router-link to="/news_post3"
+                  ><font-awesome-icon icon="fa-solid fa-chevron-right"
+                /></router-link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- <ul class="news-column">
+          <li></li>
+        </ul> -->
+
+        <div class="products-all text-end">
+          <router-link to="/shop_info"
+            ><button type="button" class="btn btn-outline-light rounded-pill">
+              All News
+            </button></router-link
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
-  components: {},
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    getProducts() {
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOM_PATH}/products`;
+      this.$http.get(api).then((response) => {
+        if (response.data.success) {
+          this.products = response.data.products.slice(2, 6);
+        }
+      });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
 };
 </script>
 
 <style scoped>
-.main1 {
+.index {
   background-color: #ec6d4e;
+  margin-top: 100px;
+}
+.content-title {
+  font-weight: 600;
+  font-size: 13px;
+  padding-top: 40px;
+  color: #fff;
+}
+
+.wh {
+  color: #fff;
+}
+.og {
+  color: #ec6d4e;
+}
+/* ----------section 1 */
+.slider {
+  /* border: 3px solid green; */
+  flex: 3;
+  overflow: hidden;
+}
+.index-slogan {
+  z-index: 1;
+  position: absolute;
+  width: 400px;
+  height: 140px;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
+.index-slogan > img {
+  display: block;
+  width: 100%;
+}
+.slider-content {
+  display: flex;
+}
+.slider-text {
+  /* border: 1px solid black; */
+  color: #fff;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+}
+.slider-text > h3 {
+  writing-mode: vertical-lr;
+  /* font-size: 24px; */
+  font-weight: 700;
+}
+.slider-text > p {
+  position: absolute;
+  left: 50px;
+  bottom: 225px;
+  font-weight: 400;
+  font-size: 13px;
+  transform-origin: left bottom;
+  transform: rotate(90deg);
+}
+.slider-img {
+  display: flex;
+  animation: scrolling 60s linear infinite;
+  height: 680px;
+}
+.slider-item {
+  flex: 0 0 24vw;
+  margin: 0 0.5vw;
+}
+.slider-item > img {
+  display: block;
+  object-fit: cover;
+  height: 680px;
+  width: 24vw;
+}
+@keyframes scrolling {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translatex(-125vw);
+    /* (16+0.5*2)*5 */
+  }
+}
+/* rotate animation */
+.scroll {
+  position: absolute;
+  width: 160px;
+  height: 160px;
+  bottom: 50px;
+  right: 80px;
+  z-index: 2;
+  animation: rotate-animation 30s infinite linear;
+}
+@keyframes rotate-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+
+/* ----------section 2 */
+.about-content {
+  height: 800px;
+  margin: 0 50px;
+  background-image: url("../assets/images/home_page/static.index__about.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.about-detail {
+  background-color: #fff;
+  color: #ec6d4e;
+  position: relative;
+  font-weight: 900;
+  width: 510px;
+  height: 700px;
+  border-radius: 10px;
+  padding: 70px 50px;
+  margin: 200px 0 0 100px;
+  z-index: 1;
+  font-size: 30px;
+}
+.detail-title {
+  font-weight: 900;
+  line-height: 1.7;
+}
+.about-detail > p {
+  padding-top: 60px;
+  font-size: 14px;
+  line-height: 2.5;
+}
+
+/* ----------section 3 */
+.marquee-content {
+  background-image: url("../assets/images/home_page/static.index__eyecatch.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 750px;
+  color: #fff;
+  /* object-fit: cover; */
+}
+.eyecatch {
+  width: 100%;
+  overflow: hidden;
+  position: absolute;
+  display: inline-block;
+  bottom: 7px;
+  left: 0;
+}
+.eyecatch > p {
+  font-weight: 400;
+  font-size: 100px;
+  line-height: 1;
+  color: #fff;
+  white-space: nowrap;
+  animation: scrolling 40s linear infinite;
+}
+
+/* ----------section 4 */
+.feature-content {
+  margin: 110px auto 0;
+  width: 90%;
+  padding: 0 50px;
+}
+.feature-list {
+  margin-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  list-style: none;
+}
+.item-number {
+  position: absolute;
+  display: block;
+  color: #fff;
+  font-size: 80px;
+  font-weight: 500;
+  line-height: 1;
+  top: -30px;
+  left: 15px;
+  z-index: 2;
+}
+.feature-item {
+  position: relative;
+  width: 33%;
+  height: auto;
+  border-radius: 5px;
+  background-color: #fff;
+}
+.feature-item > img {
+  width: 100%;
+  position: relative;
+  border-radius: 5px 5px 0 0;
+  overflow: hidden;
+  z-index: 1;
+}
+.feature-detail {
+  font-weight: 900;
+  padding: 20px 30px 30px;
+}
+.feature-title {
+  font-weight: 900;
+  font-size: 18px;
+  line-height: 1.6;
+  color: #ec6d4e;
+  text-align: center;
+  margin-bottom: 20px;
+}
+.feature-text {
+  font-size: 14px;
+}
+
+/* ----------section 5 */
+.story-content {
+  margin-top: 150px;
+  width: 100%;
+  height: 800px;
+  background-image: url("../assets/images/home_page/static.index__story-eyecatch-bg.jpg");
+  /* overflow: hidden; */
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: #fff;
+}
+.content-detail {
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+  padding: 50px 80px;
+}
+.story-detail {
+  padding-top: 8%;
+}
+.story-detail > h2 {
+  font-weight: 900;
+  line-height: 1.6;
+}
+.story-detail > p {
+  font-weight: 900;
+  line-height: 2;
+  padding: 30px 170px 0 0;
+}
+.story-btn {
+  width: 250px;
+}
+.story-img {
+  width: 90%;
+  overflow: hidden;
+  margin-left: 50px;
+  z-index: 1;
+}
+/* ----------section 6 */
+.product-content {
+  width: 100%;
+  margin: 170px auto 0;
+  padding: 50px;
+  color: #fff;
+}
+.product-container {
+  width: 100%;
+  margin: 0 auto;
+  padding: 100px 250px;
+}
+.product-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 130px;
+}
+.product-img {
+  width: 40%;
+  position: relative;
+}
+.top-img {
+  position: relative;
+  z-index: 1;
+}
+.rotate-l {
+  position: absolute;
+  transform: rotate(-20deg);
+  left: 0;
+}
+.rotate-r {
+  position: absolute;
+  transform: rotate(20deg);
+  left: 0;
+}
+.product-detail {
+  width: 40%;
+
+  padding-top: 100px;
+}
+.product-detail > h1 {
+  font-weight: 900;
+}
+.product-detail > h5 {
+  font-weight: 900;
+}
+.product-detail > p {
+  padding-top: 10px;
+  font-size: 24px;
+  line-height: 1;
+}
+/* ----------section 7 */
+.news-content {
+  max-width: 1240px;
+  width: 90%;
+  margin: 0 auto;
+  padding: 0 50px;
+}
+.news-column > tr {
+  padding: 20px 0;
 }
 </style>
