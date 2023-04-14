53ad3798b118dab7748957a98d9bd24ef5107a1f
weiwei <weiweilee826@gmail.com>
2023-04-14 16:41:29 +0800

    新增 最新消息內容*3

---
+++ src/views/NewsPost1.vue
@@ -0,0 +1,106 @@
+<template>
+  <div style="background-color: #ec6d4e">
+    <div class="container-fluid" style="padding-top: 200px">
+      <div class="container customer-card">
+        <h3 class="mb-1">
+          【從4/6開始數量有限】“FruOats”將開始銷售“素食巧克力”！
+        </h3>
+        <p>2023.04.01</p>
+        <div class="mt-5">
+          <img
+            class="img-fluid"
+            src="../assets/images/news/press1.jpg"
+            alt=""
+          />
+          <h3>純素巧克力加水果燕麥</h3>
+          <p>
+            Full Oats 出售由超級食物燕麥片、豆渣、奇亞籽和亞麻籽等 100%
+            植物來源材料製成的無麩質餅乾，以及富含膳食纖維和蛋白質等營養成分的“純素巧克力”
+            。
+          </p>
+          <img
+            class="img-fluid"
+            src="../assets/images/news/press2.jpg"
+            alt=""
+          />
+          <p>
+            為了讓任何人都能安心享用，我們對巧克力中常見的三種過敏原（乳製品、小麥和無堅果）做出了回應。
+            乾果用的很奢侈，膳食纖維、蛋白質等營養成分也很豐富。
+            享受既美味又健康的甜點。
+          </p>
+          <h3>提供兩種營養口味</h3>
+          <h5>大約 1/2 份的膳食纖維！“蔓越莓可可粒”</h5>
+          <img
+            class="img-fluid"
+            src="../assets/images/news/veganchoco7.jpg"
+            alt=""
+          />
+          <p>
+            一顆酸甜微苦的“蔓越莓可可豆粒”，提供約1/2的膳食纖維（4g）。作為超級食品而備受矚目的可可粒含有豐富的礦物質和膳食纖維等營養成分，據說具有放鬆身心的作用。可可含量約為50%。
+          </p>
+          <h5>你可以獲得4克蛋白質！“芒果椰子”</h5>
+          <img
+            class="img-fluid"
+            src="../assets/images/news/veganchoco8.jpg"
+            alt=""
+          />
+          <p>
+            可以享受熱帶風味和大豆泡芙酥脆口感的“芒果椰子”，可以攝入超過4克的蛋白質。打造美麗肌膚和秀發的蛋白質是容易缺乏的營養物質，因此推薦給正在運動的人和注重美容的人。可可含量約為34%。
+          </p>
+          <h3>一份關愛您身體的禮物或一份對您自己的獎勵</h3>
+          <img
+            class="img-fluid"
+            src="../assets/images/news/veganchoco9.jpg"
+            alt=""
+          />
+          <p>
+            “純素巧克力”是送給關心自己身體的人的完美禮物。每顆巧克力都包裹在
+            FruOats 原裝限量包裝中。此外，還免費贈送便於拿取的原創紙袋。
+            給你的朋友、家人、愛人和其他欠你恩情的人一份禮物怎麼樣？
+          </p>
+        </div>
+      </div>
+      <div class="text-center">
+        <router-link to="/latest_news"
+          ><button
+            type="button"
+            class="btn btn-outline-light btn-lg rounded-pill mt-5 fw-bold"
+          >
+            最新消息
+          </button></router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+h3 {
+  color: #ec6d4e;
+  font-weight: 900;
+  padding: 20px 0;
+}
+h5 {
+  color: #ec6d4e;
+  font-size: 16pt;
+  font-weight: bold;
+}
+button {
+  width: 250px;
+}
+.customer-card {
+  width: 950px;
+  border-radius: 10px;
+  background-color: white;
+  padding: 100px;
+}
+p {
+  font-weight: 700;
+  line-height: 2;
+}
+img {
+  padding: 20px 0;
+}
+.customer-card > p {
+  color: #ec6d4e;
+}
+</style>
---
+++ src/views/NewsPost2.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="background-color: #ec6d4e">
+    <div class="container-fluid" style="padding-top: 200px">
+      <div class="container customer-card">
+        <h3 class="mb-1">發表於2023年3月號的雜誌《Ray》</h3>
+        <p>2023.03.21</p>
+        <div class="mt-5">
+          <img class="img-fluid" src="../assets/images/news/Ray.jpg" alt="" />
+          <p>FruOats 在 2023 年 3 月 22 日發行的“Ray”月刊中有介紹。</p>
+        </div>
+      </div>
+      <div class="text-center">
+        <router-link to="/latest_news"
+          ><button
+            type="button"
+            class="btn btn-outline-light btn-lg rounded-pill mt-5 fw-bold"
+          >
+            最新消息
+          </button></router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+h3 {
+  color: #ec6d4e;
+  font-weight: 900;
+  padding: 20px 0;
+}
+button {
+  width: 250px;
+}
+.customer-card {
+  width: 950px;
+  border-radius: 10px;
+  background-color: white;
+  padding: 100px;
+}
+p {
+  font-weight: 700;
+  line-height: 2;
+}
+img {
+  padding: 20px 0;
+}
+.customer-card > p {
+  color: #ec6d4e;
+}
+</style>
